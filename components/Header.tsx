cf362656f144d0a2f7557d755cc1246a09c24760
kahyou22 <kahyou222@gmail.com>
2025-11-06 09:03:34 +0900

    style: /about 페이지 스타일 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,12 +5,14 @@ export default function Header() {
     <header>
       <nav>
         <div className="left">
-          <Image src="/logo.svg" alt="logo" width={32} height={32} preload />
-          <h1>블로그</h1>
-        </div>
-        <div className="menu">
-          <a href="/blog">blog</a>
-          <a href="/about">about</a>
+          <a className="title" href="/">
+            <Image src="/logo.svg" alt="logo" width={32} height={32} preload />
+            <h1>블로그</h1>
+          </a>
+          <div className="menu">
+            <a href="/blog">blog</a>
+            <a href="/about">about</a>
+          </div>
         </div>
         <div className="right">.</div>
       </nav>
