a553fb28de22dda672546b7f873ce1e8a7847605
kahyou22 <kahyou222@gmail.com>
2025-11-11 13:55:05 +0900

    feat: 테마 선택 컴포넌트에 ThemeProvider 훅 사용하게 변경

--- components/ThemePicker.tsx
+++ components/ThemePicker.tsx
@@ -3,17 +3,10 @@
 import { useLayoutEffect, useRef, useState } from "react";
 import Button from "./Button";
 import { Sun, Moon, MonitorCog, Check } from "lucide-react";
-import {
-  AppliedThemeId,
-  changeTheme,
-  getAppliedTheme,
-  getTheme,
-  initTheme,
-  THEME_KEY,
-  ThemeId,
-} from "@/lib/theme";
+import { ThemeId } from "@/lib/theme";
 
 import styles from "./ThemePicker.module.css";
+import { useTheme } from "@/hooks/ThemeContext";
 
 type IconType = React.ComponentType<any>;
 
@@ -29,49 +22,12 @@ const themes: ThemeItem[] = [
   { id: "system", name: "시스템 테마", icon: MonitorCog },
 ];
 
-function DangerThemeScript() {
-  const innerScript = `
-(() => {
-  let theme = localStorage.getItem("${THEME_KEY}") || "system";
-  if (theme == "system") {
-    const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    theme = systemPrefersDark ? "dark" : "light";
-  }
-  if (theme == "dark") {
-    document.documentElement.setAttribute("data-theme", theme);
-  } else {
-    document.documentElement.removeAttribute("data-theme");
-  }
-})();
-  `;
-  return (
-    <script
-      dangerouslySetInnerHTML={{
-        __html: innerScript,
-      }}
-    />
-  );
-}
-
 export default function ThemePicker() {
-  const [theme, setTheme] = useState<AppliedThemeId>("light");
-  const [selected, setSelected] = useState<ThemeId>("system");
+  const { appliedThemeId, themeId, selectThemeId } = useTheme();
   const [open, setOpen] = useState<boolean>(false);
   const wrapperRef = useRef<HTMLDivElement>(null);
 
   useLayoutEffect(() => {
-    initTheme();
-    setTheme(getAppliedTheme());
-    setSelected(getTheme());
-
-    window
-      .matchMedia("(prefers-color-scheme: dark)")
-      .addEventListener("change", () => {
-        if (getTheme() == "system") {
-          setTheme(getAppliedTheme());
-        }
-      });
-
     const handleClickOutside = (event: MouseEvent) => {
       if (wrapperRef.current?.contains(event.target as Node)) return;
       close();
@@ -88,19 +44,16 @@ export default function ThemePicker() {
   const toggle = () => setOpen((v) => !v);
 
   const choose = (id: ThemeId) => {
-    changeTheme(id);
-    setTheme(getAppliedTheme());
-    setSelected(id);
+    selectThemeId(id);
     close();
   };
 
   return (
     <>
-      <DangerThemeScript />
       <div ref={wrapperRef} className={styles.wrapper}>
         <Button variant="ghost" iconOnly onClick={toggle}>
           {themes.map((t) => {
-            if (t.id === theme) {
+            if (t.id === appliedThemeId) {
               const Icon = t.icon;
               return <Icon key={t.id} />;
             }
@@ -121,7 +74,7 @@ export default function ThemePicker() {
                 >
                   <Icon />
                   <span>{theme.name}</span>
-                  {theme.id === selected && <Check className={styles.check} />}
+                  {theme.id === themeId && <Check className={styles.check} />}
                 </Button>
               );
             })}
@@ -131,8 +84,3 @@ export default function ThemePicker() {
     </>
   );
 }
-
-// export default async function ThemePicker() {
-//   const cookieTheme = (await cookies()).get("kahyou.dev__theme")?.value;
-//   const theme = cookieTheme ? cookieTheme : "system";
-// }
