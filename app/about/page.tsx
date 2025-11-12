4eabfed60f5b569353dec9f2e5b14ebdf4751042
kahyou22 <kahyou222@gmail.com>
2025-11-13 06:03:47 +0900

    feat: /about 페이지 avatar 영역 및 이름, 설명 추가

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,8 +11,14 @@ export const metadata: Metadata = {
 
 export default function About() {
   return (
-    <div className={`md-body ${style.about}`}>
-      <AboutInfo />
+    <div className={`${style.about}`}>
+      <div className={style["avatar-wrapper"]}></div>
+      <h3>정문주</h3>
+      <h4>아마추어 소프트웨어 엔지니어</h4>
+      <hr />
+      <div className={`md-body ${style.intro}`}>
+        <AboutInfo />
+      </div>
     </div>
   );
 }
