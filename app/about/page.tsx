5b0962744f1203dbe0bb3a6141374da37ef23f3d
kahyou22 <kahyou222@gmail.com>
2025-11-13 21:59:31 +0900

    feat: /about 페이지 아바타 추가

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,6 +6,7 @@ import GmailLogo from "@/public/gmail.svg";
 import { Mail } from "lucide-react";
 
 import { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "About",
@@ -15,7 +16,9 @@ export const metadata: Metadata = {
 export default function About() {
   return (
     <div className={style.about}>
-      <div className="avatar-wrapper"></div>
+      <div className="avatar-wrapper">
+        <Image src="/pingu.webp" width={200} height={200} alt="pingu" />
+      </div>
       <h3 className="name">정문주</h3>
       <h4 className="title">아마추어 소프트웨어 엔지니어</h4>
       <div className="links">
