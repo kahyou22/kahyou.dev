3ac2770ab647c325a8946053a878374ddbfe3199
kahyou22 <kahyou222@gmail.com>
2025-11-13 09:06:34 +0900

    feat: /about 페이지 링크, 아이콘 추가 및 리팩토링

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,8 @@
 import AboutInfo from "./about.mdx";
 import "@/styles/markdown.css";
 import style from "./page.module.css";
+import GithubLogo from "@/public/github-mark.svg";
+import { Mail } from "lucide-react";
 
 import { Metadata } from "next";
 
@@ -11,12 +13,20 @@ export const metadata: Metadata = {
 
 export default function About() {
   return (
-    <div className={`${style.about}`}>
-      <div className={style["avatar-wrapper"]}></div>
-      <h3>정문주</h3>
-      <h4>아마추어 소프트웨어 엔지니어</h4>
+    <div className={style.about}>
+      <div className="avatar-wrapper"></div>
+      <h3 className="name">정문주</h3>
+      <h4 className="title">아마추어 소프트웨어 엔지니어</h4>
+      <div className="links">
+        <a href="mailto:kahyou222@gmail.com">
+          <Mail width={34} height={34} />
+        </a>
+        <a href="https://github.com/kahyou22" target="_blank">
+          <GithubLogo height={32} />
+        </a>
+      </div>
       <hr />
-      <div className={`md-body ${style.intro}`}>
+      <div className="md-body intro">
         <AboutInfo />
       </div>
     </div>
