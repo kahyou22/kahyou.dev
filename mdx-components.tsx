eab443b959d01a56b87147d54db109c3f5424d27
kahyou22 <kahyou222@gmail.com>
2025-11-17 10:41:00 +0900

    feat: MDX 설정 업데이트

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,19 @@
 import type { MDXComponents } from "mdx/types";
+import Image, { ImageProps } from "next/image";
 
 const components: MDXComponents = {};
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return components;
+  return {
+    img: (props: ImageProps) => (
+      <Image
+        width={100}
+        height={100}
+        sizes="100vw"
+        style={{ width: "100%", height: "auto" }}
+        {...props}
+      />
+    ),
+    ...components,
+  };
 }
